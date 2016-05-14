d44664a0b0fefab5f13ee61f6d97606c8b0d0709
lmeskova <meskovalu@gmail.com>
2016-05-15 01:03:18 +0200

    District seeder added

--- database/seeds/DistrictsSeeder.php
+++ database/seeds/DistrictsSeeder.php
@@ -15,23 +15,10 @@ class DistrictsSeeder extends Seeder
         $csv = array_map('str_getcsv', file(database_path('seeds/csvFiles/Districts.csv')));
 
         foreach ($csv as $row) {
-            if (isset($row[3])) {
-
-                $name = $row[0];
-                $code = $row[1];
-                $region_id = $row[2];
-
-
-                $row[0] = $name;
-                $row[1] = $code;
-                $row[2] = $region_id;
-            }
-
             District::create([
                 'name' => $row[0],
                 'code' => $row[1],
                 'region_id' => $row[2]
-
             ]);
         }
     }
