b6361008bd9de299bea2cc7fbf33374ae7a0f212
lmeskova <meskovalu@gmail.com>
2016-05-18 23:27:44 +0200

    Editacia incident detailu

--- app/Http/Controllers/IncidentDetailController.php
+++ app/Http/Controllers/IncidentDetailController.php
@@ -22,6 +22,8 @@ use App\InitiatorsImpact;
 use App\OrganizationShortcoming;
 use App\SpreadingCause;
 use App\VehiclePart;
+use Illuminate\Support\Facades\Validator;
+use Laracasts\Flash\Flash;
 
 
 class IncidentDetailController extends Controller
@@ -90,9 +92,41 @@ class IncidentDetailController extends Controller
     public function store($incidentId, Request $request)
     {
 
+
         $incident = Incident::findOrFail($incidentId);
 
+
+
+        $validator = Validator::make($request->all(), [
+            "area_id" => "numeric",
+            "fire_location_id" => "numeric",
+            "vehicle_part_id" => "numeric",
+            "conveyor_equipment_id" => "numeric",
+            "incident_cause_id" => "numeric",
+            "flammable_substance_id" => "numeric",
+            "initiator_id" => "numeric",
+            "electrical_wiring_id" => "numeric",
+            "initiators_impact_id" => "numeric",
+            "burning_substance_id" => "numeric",
+            "following_substance_id" => "numeric",
+            "hazard_class_id" => "numeric",
+            "organization_shortcoming_id" => "numeric",
+            "action_shortcoming_id" => "numeric",
+            "incident_conclusion_id" => "numeric",
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+
+
         $incident->incidentDetail()->create($request->all());
+
+        Flash::success('Detail incidentu úspešne vytvorený :)');
 
         return redirect()->route('dashboard');
     }
@@ -113,24 +147,97 @@ class IncidentDetailController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param $incidentId
+     * @param $incidentDetailId
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($incidentId, $incidentDetailId)
     {
-        //
+        $incidentDetail = IncidentDetail::findOrFail($incidentDetailId);
+
+        $areas = Area::all();
+        $fireLocations = FireLocation::all();
+        $vehicleParts = VehiclePart::all();
+        $conveyorEquipments = ConveyorEquipment::all();
+        $incidentCauses = IncidentCause::all();
+        $flammableSubstances = FlammableSubstance::all();
+        $initiators = Initiator::all();
+        $electricalWirings = ElectricalWiring::all();
+        $initiatorsImpacts = InitiatorsImpact::all();
+        $burningSubstances = BurningSubstance::all();
+        $followingSubstances = FollowingSubstance::all();
+        $hazardClasses = HazardClass::all();
+        $organizationShortcomings = OrganizationShortcoming::all();
+        $actionShortcomings = ActionShortcoming::all();
+        $incidentConclusions = IncidentConclusion::all();
+
+
+        return view('incidentDetail.edit', [
+            'areas' => $areas,
+            'fireLocations' => $fireLocations,
+            'vehicleParts' => $vehicleParts,
+            'conveyorEquipments' => $conveyorEquipments,
+            'incidentCauses' => $incidentCauses,
+            'flammableSubstances' => $flammableSubstances,
+            'initiators' => $initiators,
+            'electricalWirings' => $electricalWirings,
+            'initiatorsImpacts' => $initiatorsImpacts,
+            'burningSubstances' => $burningSubstances,
+            'followingSubstances' => $followingSubstances,
+            'hazardClasses' => $hazardClasses,
+            'organizationShortcomings' => $organizationShortcomings,
+            'actionShortcomings' => $actionShortcomings,
+            'incidentConclusions' => $incidentConclusions,
+            'incidentDetail' => $incidentDetail,
+
+
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param $incidentId
+     * @param $incidentDetailId
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $incidentId, $incidentDetailId)
     {
-        //
+        $incidentDetail = IncidentDetail::findOrFail($incidentDetailId);
+
+        $validator = Validator::make($request->all(), [
+            "area_id" => "numeric",
+            "fire_location_id" => "numeric",
+            "vehicle_part_id" => "numeric",
+            "conveyor_equipment_id" => "numeric",
+            "incident_cause_id" => "numeric",
+            "flammable_substance_id" => "numeric",
+            "initiator_id" => "numeric",
+            "electrical_wiring_id" => "numeric",
+            "initiators_impact_id" => "numeric",
+            "burning_substance_id" => "numeric",
+            "following_substance_id" => "numeric",
+            "hazard_class_id" => "numeric",
+            "organization_shortcoming_id" => "numeric",
+            "action_shortcoming_id" => "numeric",
+            "incident_conclusion_id" => "numeric",
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+
+        $incidentDetail->update($request->all());
+
+        Flash::success('Detail incidentu úspešne upraveny :)');
+
+        return redirect()->route('dashboard');
+
     }
 
     /**
