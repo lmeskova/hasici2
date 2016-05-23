a56bf77c19d6e5063cb7dd7a0f5e0cc6e6673360
lmeskova <meskovalu@gmail.com>
2016-05-23 12:14:08 +0200

    dashboard edited

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Prihlásiť
+                                    <i class="fa fa-btn fa-sign-in"></i> Prihlásiť
                                 </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Zabudli ste heslo?</a>
--- resources/views/incident/show.blade.php
+++ resources/views/incident/show.blade.php
@@ -8,56 +8,55 @@
 
             <table class="table table-striped">
                 <tr >
-                    <td >Evc</td>
+                    <td >Evidenčné číslo</td>
                     <td >{{$incident->evidence_number}}</td>
                 </tr>
                 <tr >
-                    <td >Town</td>
-                    <td >{{$incident->town->name}}</td>
-                </tr>
-                <tr >
-                    <td >address</td>
-                    <td >{{$incident->address}}</td>
-                </tr>
-                <tr >
-                    <td >Vlastnictvo</td>
-                    <td >{{ $incident->ownership ? $incident->ownership->name : null}}</td>
-                </tr>
-                <tr >
-                    <td >Urcenie skody</td>
-                    <td >{{$incident->damageSpecification->name}}</td>
-                </tr>
-                <tr >
-                    <td >Typ poskodenia</td>
-                    <td >{{$incident->damageType->name}}</td>
-                </tr>
-                <tr >
-                    <td >Industry type</td>
-                    <td >{{$incident->industryType->name}}</td>
-                </tr>
-                <tr >
-                    <td >Dir dmg</td>
-                    <td >{{$incident->direct_damage_value}}</td>
-                </tr>
-                <tr >
-                    <td >folu dmg</td>
-                    <td >{{$incident->followup_damage_value}}</td>
-                </tr>
-                <tr >
-                    <td >saved value</td>
-                    <td >{{$incident->saved_value}}</td>
-                </tr>
-                <tr >
-                    <td >Obsrv date</td>
+                    <td >Deň a čas spozorovania</td>
                     <td >{{$incident->observe_date}}</td>
                 </tr>
                 <tr >
-                    <td >Rep date</td>
+                    <td >Deň a čas ohlásenia</td>
                     <td >{{$incident->report_date}}</td>
                 </tr>
-
                 <tr >
-                    <td >Poistovne</td>
+                    <td >Obec</td>
+                    <td >{{$incident->town->name}}</td>
+                </tr>
+                <tr >
+                    <td >Adresa</td>
+                    <td >{{$incident->address}}</td>
+                </tr>
+                <tr >
+                    <td >Vlastníctvo</td>
+                    <td >{{ $incident->ownership ? $incident->ownership->name : null}}</td>
+                </tr>
+                <tr >
+                    <td >Určenie škody</td>
+                    <td >{{$incident->damageSpecification->name}}</td>
+                </tr>
+                <tr >
+                    <td >Charakter škôd</td>
+                    <td >{{$incident->damageType->name}}</td>
+                </tr>
+                <tr >
+                    <td >Odvetvie ekonomickej činnosti</td>
+                    <td >{{$incident->industryType->name}}</td>
+                </tr>
+                <tr >
+                    <td >Priama škoda</td>
+                    <td >{{$incident->direct_damage_value}}</td>
+                </tr>
+                <tr >
+                    <td >Následná škoda</td>
+                    <td >{{$incident->followup_damage_value}}</td>
+                </tr>
+                <tr >
+                    <td >Uchrnánené hodnoty</td>
+                    <td >{{$incident->saved_value}}</td>
+                </tr>
+                <tr >
+                    <td >Poisťovne</td>
                     <td     >
                         <div class="row">
                             @forelse($incident->insuranceCompanies as $company)
@@ -65,17 +64,17 @@
                                     {{$company->name}}
                                 </div>
                             @empty
-                                <div class="col-lg-12">ziadne poistovne</div>
+                                <div class="col-lg-12">Bez poisťovne</div>
                             @endforelse
                         </div>
                     </td>
                 </tr>
+            <tr >
+                <td >Fotografická dokumentácia</td>
+                <td > </td>
+            </tr>
             </table>
 
-
-            <div class="row">
-                <div class="col-lg-5">Obrazky</div>
-            </div>
             <div class="row">
                 @foreach($incident->images as $image)
                     <div class="col-lg-3">
@@ -92,18 +91,64 @@
 
             <table class="table table-striped">
                 <tr >
-                    <td >Area</td>
-                    <td >{{$incident->incidentDetail->area ? $incident->incidentDetail->area->name : 'N/A'}}</td>
+                    <td >Priestor</td>
+                    <td >{{$incident->incidentDetail->area ? $incident->incidentDetail->area->name : '-'}}</td>
                 </tr>
                 <tr >
-                    <td >Fire location</td>
-                    <td >{{$incident->incidentDetail->fireLocation ? $incident->incidentDetail->fireLocation->name : 'N/A'}}</td>
-                </tr>                <tr >
-                    <td >Vehicle part</td>
-                    <td >{{$incident->incidentDetail->vehiclePart ? $incident->incidentDetail->vehiclePart->name : 'N/A'}}</td>
-                </tr>                <tr >
-                    <td >Conveyor</td>
-                    <td >{{$incident->incidentDetail->conveyorEquipment ? $incident->incidentDetail->conveyorEquipment->name : 'N/A'}}</td>
+                    <td >Miesto vzniku požiaru</td>
+                    <td >{{$incident->incidentDetail->fireLocation ? $incident->incidentDetail->fireLocation->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Časť dopravného prostriedku a pracovného stroja</td>
+                    <td >{{$incident->incidentDetail->vehiclePart ? $incident->incidentDetail->vehiclePart->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Dopravníkové zariadenia</td>
+                    <td >{{$incident->incidentDetail->conveyorEquipment ? $incident->incidentDetail->conveyorEquipment->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Príčina vzniku požiaru</td>
+                    <td >{{$incident->incidentDetail->incidentCause ? $incident->incidentDetail->incidentCause->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Manipulácia s horľavými výbušnými látkami</td>
+                    <td >{{$incident->incidentDetail->flammableSubstances ? $incident->incidentDetail->flammableSubstances->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Iniciátor</td>
+                    <td >{{$incident->incidentDetail->initiator ? $incident->incidentDetail->initiator->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Časti elektrických rozvodov</td>
+                    <td >{{$incident->incidentDetail->electricalwiring ? $incident->incidentDetail->electricalwiring->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Pôsobenie iniciátora</td>
+                    <td >{{$incident->incidentDetail->initiatorsImpact ? $incident->incidentDetail->initiatorsImpact->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Látky, ktoré začali horieť ako prvé</td>
+                    <td >{{$incident->incidentDetail->burningSubstance ? $incident->incidentDetail->burningSubstance->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Látky, ktoré určovali rozvoj</td>
+                    <td >{{$incident->incidentDetail->followingSubstance ? $incident->incidentDetail->followingSubstance->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Trieda nebezpečnosti horľavých kvapalín</td>
+                    <td >{{$incident->incidentDetail->hazardClass ? $incident->incidentDetail->hazardClass->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Nedostatky v organizácii s vplyvom na vznik požiaru</td>
+                    <td >{{$incident->incidentDetail->incidentCause ? $incident->incidentDetail->incidentCause->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Nedostatky v činnostiach s vplyvom na šírenie požiaru</td>
+                    <td >{{$incident->incidentDetail->incidentCause ? $incident->incidentDetail->incidentCause->name : '-'}}</td>
+                </tr>
+                <tr >
+                    <td >Uzatvorenie prípadu</td>
+                    <td >{{$incident->incidentDetail->incidentCause ? $incident->incidentDetail->incidentCause->name : '-'}}</td>
                 </tr>
             </table>
 
@@ -120,9 +165,9 @@
             @endif
 
             @if($incident->damagedObject)
-                <a href="{{route('incident.incidentDamagedObject.edit', [$incident->id, $incident->damagedObject->id])}}" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-pencil"></span> Uprav detail požiaru</a>
+                <a href="{{route('incident.incidentDamagedObject.edit', [$incident->id, $incident->damagedObject->id])}}" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-pencil"></span> Uprav poškodený objekt</a>
             @else
-                <a href="{{route('incident.incidentDamagedObject.create', [$incident->id])}}" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span> Pridaj detail požiaru</a>
+                <a href="{{route('incident.incidentDamagedObject.create', [$incident->id])}}" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span> Pridaj poškodený objekt</a>
             @endif
         </div>
     </div>
